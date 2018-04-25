c5f0961c6dd9c9efa7b2f3b4c5990cd1e99f4f7d
Rafael Felipe <rafafelipesilva@gmail.com>
2018-04-25 12:09:27 -0300

    first

---
+++ app/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const authController = require('./controllers/authController');
+
+routes.get('/', authController.signin);
+routes.get('/signup', authController.signup);
+
+routes.post('/register', authController.register);
+
+
+module.exports = routes;
