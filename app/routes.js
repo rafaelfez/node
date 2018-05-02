6f597f8011657cc9f30b122242cd378f0ab2c664
Rafael Felipe <rafafelipesilva@gmail.com>
2018-05-02 20:59:50 -0300

    finish

--- app/routes.js
+++ app/routes.js
@@ -43,7 +43,10 @@ routes.post('/app/categories/create', categoryController.store);
 /**
  * Snippets
  */
+routes.get('/app/categories/:categoryId/snippets/:id', snippetController.show);
 routes.post('/app/categories/:categoryId/snippets/create', snippetController.store);
+routes.put('/app/categories/:categoryId/snippets/:id', snippetController.update);
+routes.delete('/app/categories/:categoryId/snippets/:id', snippetController.destroy);
 
 routes.use((req, res) => res.render('errors/404'));
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const routes = require('./app/routes');
 const session = require('express-session');
 const flash = require('connect-flash');
+const methodOverride = require('method-override');
 
 const sessionConfig = require('./config/session');
 
@@ -21,6 +22,7 @@ app.set('view engine', 'njk');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(methodOverride('_method'));
 
 app.use('/', routes);
 
